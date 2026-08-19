284d3ffe7c72422c9e28faa87c5da30b54ee8bbc
vietnam-nightlife <kimjongseo0403@gmail.com>
2026-08-19 22:01:56 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,11 @@
+import type { MetadataRoute } from "next";
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://thailandboomnightlife.com/sitemap.xml",
+  };
+}
