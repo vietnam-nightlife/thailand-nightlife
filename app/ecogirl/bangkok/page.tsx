1a986436c6ca496d8f2621afb9d82fb95fbd61ce
vietnam-nightlife <kimjongseo0403@gmail.com>
2026-08-31 01:05:49 +0700

    Update page.tsx

--- app/ecogirl/bangkok/page.tsx
+++ app/ecogirl/bangkok/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const GITHUB_IMAGE =
   "https://raw.githubusercontent.com/vietnam-nightlife/thailand-nightlife/main/app/ecogirl/bangkok";
 
-const profiles = Array.from({ length: 18 }, (_, i) => {
+const profiles = Array.from({ length: 39 }, (_, i) => {
   const number = i + 1;
 
   return {
